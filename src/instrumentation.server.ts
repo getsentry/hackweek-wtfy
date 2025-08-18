1a564875fc30a2b46cab63eb400dc356d41008cb
Lukas Stracke <lukas.stracke@sentry.io>
2025-08-18 13:19:53 +0200

    add sentry

---
+++ src/instrumentation.server.ts
@@ -0,0 +1,10 @@
+import * as Sentry from '@sentry/sveltekit';
+
+console.log('Initializing Sentry');
+
+Sentry.init({
+	dsn: 'https://f9ad87a00149a3d6faea76ebfc2d1483@o447951.ingest.us.sentry.io/4509864603484160',
+	tracesSampleRate: 1.0,
+	enableLogs: true
+	// debug: true
+});
