4bec4c624f46fa42147a9736211915012f6fb4c2
Lukas Stracke <lukas.stracke@sentry.io>
2025-08-20 13:21:26 +0200

    more debug

--- src/routes/auth/login/+server.ts
+++ src/routes/auth/login/+server.ts
@@ -2,11 +2,12 @@ import { redirect } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 import { getGitHubOAuthUrl } from '$lib/server/auth.js';
 
-export const GET: RequestHandler = async ({ url }) => {
+export const GET: RequestHandler = async ({ url, request }) => {
 	// Build the callback URL
 	const redirectUri = `${url.origin}/auth/callback`;
 
 	console.log('xx', { redirectUri });
+	console.log('xx2', request);
 
 	// Generate GitHub OAuth URL
 	const oauthUrl = getGitHubOAuthUrl(redirectUri);
