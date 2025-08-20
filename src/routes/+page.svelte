ce9800718555d82ee5ac866a7f97b45fc4c95288
Lukas Stracke <lukas.stracke@sentry.io>
2025-08-20 16:17:18 +0200

    s/analyses/queries

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -450,7 +450,7 @@
 					class="animate-in fade-in-0 slide-in-from-bottom-4 sticky top-8 h-fit rounded-lg bg-white shadow-lg delay-300 duration-500 dark:bg-gray-800"
 				>
 					<div class="border-b border-gray-200 p-4 dark:border-gray-700">
-						<h3 class="text-lg font-medium text-gray-900 dark:text-white">📋 Recent Analyses</h3>
+						<h3 class="text-lg font-medium text-gray-900 dark:text-white">📋 Recent Queries</h3>
 						<p class="mt-1 text-xs text-gray-500 dark:text-gray-400">Last 5 • Click to reuse</p>
 					</div>
 					<div class="p-4">
