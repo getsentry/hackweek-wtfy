892065ddffbaf9ff399cd6209cd05c4180cfbe67
Lukas Stracke <lukas.stracke@sentry.io>
2025-08-20 13:44:55 +0200

    fix redirect url pls?

--- src/routes/auth/login/+server.ts
+++ src/routes/auth/login/+server.ts
@@ -1,10 +1,11 @@
 import { redirect } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 import { getGitHubOAuthUrl } from '$lib/server/auth.js';
+import { dev } from '$app/environment';
 
 export const GET: RequestHandler = async ({ url, request }) => {
 	// Build the callback URL
-	const redirectUri = `${url.origin}/auth/callback`;
+	const redirectUri = `${dev ? `http://localhost:${url.port}` : 'https://wtfy.sentry.gg'}/auth/callback`;
 
 	console.log('xx', { redirectUri });
 	console.log('xx2', request);
